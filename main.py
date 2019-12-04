6ab58eb43c29b017b7ec0d33efd177d9dc2c0620
PJRaj <vijayrajshanmugarajt@gmail.com>
2019-12-05 01:47:59 +0530

    main.py

--- main.py
+++ main.py
@@ -8,6 +8,9 @@ def start_message():
     print('>> 2. Edit existing file')
     print('>> 3. Delete file')
 
+def log_edit(filename):
+    pass
+
 def main():
     # Test commit
     start_message()
@@ -17,16 +20,38 @@ def main():
 
     while(not stat):
         file_list = os.listdir('./Files')
-        stat = input('>> ')
+        stat = input('>>')
+
         if stat == 1:
             name = input('>> File name:')
+            if name not in file_list:
+                log_edit(name)
+            else:
+                cout('LaTEXlogError: File already exists')
+        
         elif stat == 2:
+            print('>>')
+            print('\n'.join(['. '+file for file in file_list]))
             
+            name = input('>> File name:')
+            if name in file_list:
+                log_edit(name)
+            else:
+                cout('LaTEXlogError: File non-existent')
+        
         elif stat == 3:
             name = input('>> File name:')
             
-        else:
+            if name not in file_list:
+                s = input('Are you sure you want to delete \''+name+'\'? (y/n)').lower()
+                if s == 'y':
+                    # Delete file
+                    pass
+            else:
+                cout('LaTEXlogError: File already exists')
             
+        else:
+            print('LaTEXlogError: Invalid input')
 
 if __name__ == '__main__':
     main()
