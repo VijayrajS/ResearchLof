4545a49c9f7b307ed902c8001c0b7aacd60f28ff
PJRaj <vijayrajshanmugarajt@gmail.com>
2019-12-07 01:23:50 +0530

    continue

--- main.py
+++ main.py
@@ -6,8 +6,7 @@ from collections import namedtuple
 
 # Declaring fields of a single record and a named tuple
 
-todo_f  = ['ID', 'Title', 'isBullets', 'remarks', 
-        'checkList', 'images', 'captions']
+todo_f  = ['ID', 'Title', 'remarks', 'checkList', 'images', 'captions']
 
 Log_r = namedtuple('L_rec', todo_f)
 
@@ -21,15 +20,26 @@ def start_message():
 
 def log_edit(filename, mode):
     
-    mode = 'w+' if mode == 1 else 'a+'
-    fp = open(filename, mode)
+    mode_st = 'w+' if mode == 1 else 'a+'
+    print(filename)
+    fp = open('./Files/' + filename, mode_st)
     
     latest_index = 0
     log_json = []
     
     fp.seek(0)
+    if mode != 1:
+        for line in fp.readlines():
+            log_json.append(json.loads(line))
+        
+        if log_json:
+            latest_index = log_json[-1]['ID']
     
-    
+    print(log_json)
+    print(latest_index)
+    close = 0
+    while not close:
+        break
 
 def main():
 
@@ -44,7 +54,7 @@ def main():
         if stat == 1:
             name = input('>> File name:')
             if name not in file_list:
-                log_edit(name, status)
+                log_edit(name, stat)
             else:
                 print('LaTEXlogError: File already exists')
         
@@ -54,7 +64,7 @@ def main():
             
             name = input('>>> File name:')
             if name in file_list:
-                log_edit(name, status)
+                log_edit(name, stat)
             else:
                 print('LaTEXlogError: File non-existent')
                 stat = 0
